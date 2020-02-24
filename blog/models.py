bb894ca09593f93e5de2427f5c235ed1c746d439
lakshitsaini <lakshitsaini2017@gmail.com>
2020-02-24 23:15:21 +0530

    blog functionality

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 class blog(models.Model):
 	title=models.CharField(max_length=200)
-	pub_date=models.DateTimeField
+	#pub_date=models.DateTimeField
 	body=models.TextField()
 	image=models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
 # Create your models here.
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howṣto/static-files/
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'portfolio/static/')
 
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
